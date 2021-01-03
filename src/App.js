9a67fcd01945f8673cd55befe900e8c51c4e6d3f
abbassiddiqui99 <abbassiddiqui99@gmail.com>
2021-01-03 18:45:02 +0500

    initialize redux

--- src/App.js
+++ src/App.js
@@ -2,12 +2,9 @@ import Login from "./Login";
 import { useStateValue } from "./StateProvider";
 
 function App() {
-  // const [user, setUser] = useState(null);
-  const [state, dispatch] = useStateValue();
-
   return (
     <div className="app">
-      {state.user ? `the user ${state.user} is loggedIN` : `u r not logged in`}
+      <h1>the user is ...</h1>
       <Login />
     </div>
   );
